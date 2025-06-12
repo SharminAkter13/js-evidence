79e1c7e681be843adc1c589ab75488cc3703ec29
SharminAkter13 <sharminakter13092000@gmail.com>
2025-06-12 23:55:31 +0600

    new

--- f-script.js
+++ f-script.js
@@ -17,7 +17,7 @@ function chEmail(){
 function chname(){
     var name = document.getElementById("tname");
      var emailNaN = /^[0-9A-Za-z]{4,11}$/;
-    if (!emailNaN.test(email.value))
+    if (!emailNaN.test(name.value))
     {
         alert("Please enter minimum 4 and max 11");
         document.getElementById("tname").style.borderColor="#ff0000";
@@ -28,4 +28,46 @@ function chname(){
 
     }
     return true;
+}
+
+function form_window(){
+    var title = "Title";
+    var name =document.f.tname.value;
+        var contact =document.f.contact.value;
+        var email =document.f.email.value;
+    var address =document.f.address.value;
+    var session =document.f.session.value;
+    var gender ="";
+    var subject ="";
+
+  //check if radio button is selected and read selected value 
+
+  for (i=0;i<document.f.gen.length;i++){
+    if(document.f.gen[i].checked){
+        gender = document.f.gen[i].value;
+    }
+  }
+
+  for (j=0;j<document.f.sub.length;j++){
+    if(document.f.sub[j].checked){
+        subject = document.f.sub[j].value;
+    }
+  }
+
+     // create new window here
+
+     a =open(",",'width=200px,height=200px' )
+     
+     with(a.document){
+     write("<html>");
+     write("<head><title>"+title+"</title></head>");
+     write("<body style='background-color:red'>");
+     write("<div style='border:1px solid gray'>Name:"+name+"<br/>");
+     write("Address:"+address+"<br/>");
+     write("Session:"+session+"<br/>");
+     write("Gender:"+gender+"<br/>");
+     write("Subject:"+subject+"<br/>");
+       write("<input type='button' value='Close me' onclick='self.close();' />");
+    write("<input type='button' value='Print' onclick='self.print();' />");
+  }
 }
